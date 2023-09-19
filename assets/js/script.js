be8bbe3e92b7d6a75bc1e40739de60236668fdc7
kazi kuete charnel <chanelkuete2@gmail.com>
2023-09-19 22:13:12 +0100

    wip

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+function calcul(DC1,DC2,Ds,result) {
+   DC1 = Number(document.getElementById("number1").value);
+   DC2 = Number(document.getElementById("number2").value);
+    Ds = Number(document.getElementById("number3").value);
+
+   result = DC1 + DC2 + Ds;
+  document.getElementById("resultat").value= result;
+
+}
+let btn = document.querySelector("#send");
+btn.addEventListener("click", calcul);
+
