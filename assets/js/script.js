3761916ac8f86e6dbe44c4cb4e02f5f3d9ebdecf
kazi kuete charnel <chanelkuete2@gmail.com>
2023-09-20 20:35:50 +0100

    dev

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,13 @@
 function tableMultiplication(nombre) {
 
   let i;
-  for (i = 1; i <= 10 ; i++) {
+  for (i = 0; i <= 9 ; i++) {
     resultat = nombre * i;
-     console.log(resultat);
   }
-  
     nombre = Number(document.getElementById("number1").value)
-    document.querySelector("#result").value = resultat
+    document.querySelector("#result").value = `${nombre} * ${i} = ${resultat}`
 }
 
-let btn = document.querySelector("submit")
+let btn = document.querySelector("#submit")
 btn.addEventListener("click",tableMultiplication)
+console.log(tableMultiplication())
