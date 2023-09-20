eaa50e0f0b64ffe2ae711de70832ad0158b5fa2e
kazi kuete charnel <chanelkuete2@gmail.com>
2023-09-20 14:07:29 +0100

    exo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,3 @@
-// function calcul(DC1,DC2,Ds,result) {
-//    DC1 = Number(document.getElementById("number1").value);
-//    DC2 = Number(document.getElementById("number2").value);
-//     Ds = Number(document.getElementById("number3").value);
-
-//    result = DC1 + DC2 + Ds;
-//   document.getElementById("resultat").value= result;
-
-// }
-// let btn = document.querySelector("#send");
-// btn.addEventListener("click", calcul);
 
 
 function calculNombreJour(month,year){
