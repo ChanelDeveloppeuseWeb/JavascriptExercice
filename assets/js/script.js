61b2e8dc1847959187cf7e648ed98aa7ad64efa1
kazi kuete charnel <chanelkuete2@gmail.com>
2023-09-20 18:21:21 +0100

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,52 @@
-function calculNombreJour(month,year){
-month = month-1
-let date = new date(year,month)
+// function calculNombreJour(month){
+// if (month == 4, 6, 9, 11 ){
 
-// En javascript les mois commence de 0-11
+// document.querySelector("#valider").value ="le nombre de jour est de 30 jours"
 
-date.setMonth(date.getMonth() + 1);
-date.setDate(date.getDate() - 1);
-let days = date.getDate();
-month = document.getElementById("mois").value
- year = document.getElementById("annee").value
- document.querySelector("#valider").value = days
- console.log(days)
-// on recupere le jour du mois avec la methode getDate()
-// setMonth permet de passer au mois suivant
-// setdate pour mettre la date au dernier jour du mois
+// }else if(month ==  1, 3, 5, 7, 8, 10 , 12){
 
- 
+// document.querySelector("#valider").value ="le nombre de jour est de 31 jours"
+
+// }
+// else{
+// document.querySelector("#valider").value ="le nombre de jour est de 28 jours"
+
+// }
+// }
+// let btn = document.querySelector("#submit")
+// btn.addEventListener("click",calculNombreJour)
+
+function calculNombreJour(Nj,month,annee){
+    month = Number(document.getElementById("mois").value)
+    annee = Number(document.getElementById("annee").value)
+    switch(month){
+        case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+                case 10:
+                    case 12:
+                        Nj= 31;
+                        break;
+                        case 4:
+                        case 6:
+                        case 9:
+                            case 11:
+                                Nj = 30;
+                                break;
+                                case 2:
+                                    if(annee%4==0){
+                                        Nj= 29 
+                                    }else{
+                                        Nj=28
+                                    }
+    }
+        document.querySelector("#valider").value = "le nombre de jour est " + Nj
+
 }
 let btn = document.querySelector("#submit")
 btn.addEventListener("click",calculNombreJour)
-
 
 
 
