f6db4c80142d6fbdd135aee5a7ff6783410f02bd
kazi kuete charnel <chanelkuete2@gmail.com>
2023-09-20 21:11:59 +0100

    wip

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
-function calcul(DC1) {
-   DC1 = Number(document.getElementById("number1").value);
-   carre = DC1 ;
-  document.getElementById("resultat").value= `le carre est:`;
-
+function carreNumber(nombre,carre) {
+   nombre = Number(document.getElementById("number1").value);
+   carre = Math.pow(nombre,2) ;
+  document.getElementById("resultat").value= `le carre est : ${carre}`;
 }
 let btn = document.querySelector("#send");
-btn.addEventListener("click", calcul);
+btn.addEventListener("click", carreNumber);
+
 
