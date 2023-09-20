17f5e43e80fa40c59c1ce7ceee697b6101e8c595
kazi kuete charnel <chanelkuete2@gmail.com>
2023-09-20 20:43:09 +0100

    wip

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
-function calcul(DC1,DC2,Ds,result) {
+function moyenne(DC1,DC2,Ds,result,somme) {
    DC1 = Number(document.getElementById("number1").value);
    DC2 = Number(document.getElementById("number2").value);
     Ds = Number(document.getElementById("number3").value);
 
    result = DC1 + DC2 + Ds;
-  document.getElementById("resultat").value= result;
+   somme = result/3
+  document.getElementById("resultat").value= `la moyenne est de : ${somme}`;
 
 }
 let btn = document.querySelector("#send");
-btn.addEventListener("click", calcul);
+btn.addEventListener("click", moyenne);
 
