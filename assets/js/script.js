51a57745d6233abd2857b0c9ec16219686c80b23
kazi kuete charnel <chanelkuete2@gmail.com>
2023-09-20 16:04:51 +0100

    rip

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,18 @@
-function calcul(DC1,DC2,Ds,result) {
-   DC1 = Number(document.getElementById("number1").value);
-   DC2 = Number(document.getElementById("number2").value);
-    Ds = Number(document.getElementById("number3").value);
+// function multiplication(number) {
+//     for (let i = 1; i <= 10; i++) {
+//       let result = number * i;
+//     console.log(result);
 
-   result = DC1 + DC2 + Ds;
-  document.getElementById("resultat").value= result;
+//     }
 
-}
-let btn = document.querySelector("#send");
-btn.addEventListener("click", calcul);
+//   }
 
+  function table_multiplication(nombre)
+		{
+
+		let i;
+		for (i=1; i <= 10; i++) {
+		    resultat=nombre*i;
+			console.log(nombre*i+ "<br>");
+		}
+		}					
