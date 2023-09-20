598c0fdc46b731cdef4f45df9012cfc7a7c4a577
kazi kuete charnel <chanelkuete2@gmail.com>
2023-09-20 14:30:21 +0100

    install

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,13 @@
 
 
-// function calculNombreJour(month,year){
-// month = month-1
-// // En javascript les mois commence de 0-11
-
-// let date = getDaysInMonth(year,month)
-
-// date.setMonth(date.getMonth() + 1);
-// date.setDate(date.getDate() - 1);
-// let days = date.getDate();
-// // on recupere le jour du mois avec la methode getDate()
-
-//  month = document.getElementById("mois").value
-//  year = document.getElementById("annee").value
-//  document.querySelector("#valider").value = days
-//  console.log(date)
-// }
-// let btn = document.querySelector("#submit")
-// btn.addEventListener("click",calculNombreJour)
-
-
+function calcul(DC1,DC2,Ds,result) {
+    DC1 = Number(document.getElementById("number1").value);
+    DC2 = Number(document.getElementById("number2").value);
+     Ds = Number(document.getElementById("number3").value);
+ 
+    result = DC1 + DC2 + Ds;
+   document.getElementById("resultat").value= result;
+ 
+ }
+ let btn = document.querySelector("#send");
+ btn.addEventListener("click", calcul);
