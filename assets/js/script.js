ed059d0c9fb69e9bf61184328903e98100673c64
kazi kuete charnel <chanelkuete2@gmail.com>
2023-09-20 19:02:19 +0100

    push

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,15 @@
-// function multiplication(number) {
-//     for (let i = 1; i <= 10; i++) {
-//       let result = number * i;
-//     console.log(result);
 
-//     }
+function tableMultiplication(nombre) {
 
-//   }
+  let i;
+  for (i = 1; i <= 10 ; i++) {
+    resultat = nombre * i;
+     console.log(resultat);
+  }
+  
+    nombre = Number(document.getElementById("number1").value)
+    document.querySelector("#result").value = resultat
+}
 
-  function table_multiplication(nombre)
-		{
-
-		let i;
-		for (i=1; i <= 10; i++) {
-		    resultat=nombre*i;
-			console.log(nombre*i+ "<br>");
-		}
-		}					
+let btn = document.querySelector("submit")
+btn.addEventListener("click",tableMultiplication)
