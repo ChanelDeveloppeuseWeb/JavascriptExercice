322cfd1c6e5d9beab46ea9e2a4dc1d538fa5bd5c
kazi kuete charnel <chanelkuete2@gmail.com>
2023-09-20 14:18:40 +0100

    exo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,22 @@
 
 
-function calculNombreJour(month,year){
-month = month-1
-let date = getDaysInMonth(year,month)
-date.setMonth(date.getMonth() + 1);
-date.setDate(date.getDate() - 1);
-let days = date.getDate();
-// on recupere le jour du mois avec la methode getDate
+// function calculNombreJour(month,year){
+// month = month-1
+// // En javascript les mois commence de 0-11
 
- month = document.getElementById("mois").value
- year = document.getElementById("annee").value
- document.querySelector("#valider").value = days
- console.log(date)
-}
-let btn = document.querySelector("#submit")
-btn.addEventListener("click",calculNombreJour)
+// let date = getDaysInMonth(year,month)
+
+// date.setMonth(date.getMonth() + 1);
+// date.setDate(date.getDate() - 1);
+// let days = date.getDate();
+// // on recupere le jour du mois avec la methode getDate()
+
+//  month = document.getElementById("mois").value
+//  year = document.getElementById("annee").value
+//  document.querySelector("#valider").value = days
+//  console.log(date)
+// }
+// let btn = document.querySelector("#submit")
+// btn.addEventListener("click",calculNombreJour)
 
 
