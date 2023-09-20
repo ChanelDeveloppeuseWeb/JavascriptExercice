ef73c09f169886f0ae70a32f683e4c98a6ac7fb6
kazi kuete charnel <chanelkuete2@gmail.com>
2023-09-20 21:32:43 +0100

    et

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,22 @@
-function calcul(DC1,DC2,Ds,result) {
+function comparaisonNumber(DC1,DC2,Ds,result,comparaison) {
    DC1 = Number(document.getElementById("number1").value);
    DC2 = Number(document.getElementById("number2").value);
     Ds = Number(document.getElementById("number3").value);
 
+    switch (comparaison) {
+      case (DC1===DC2 && DC2===Ds):
+        result="DC1,DC2,Ds sont egales"
+        
+        break;
+      case (DC1==DC2 || DC2==Ds);
+      break;
+      default:
+        break;
+    }
    result = DC1 + DC2 + Ds;
   document.getElementById("resultat").value= result;
 
 }
 let btn = document.querySelector("#send");
-btn.addEventListener("click", calcul);
+btn.addEventListener("click", comparaisonNumber);
 
