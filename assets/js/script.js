34cbf067b86c1b71f60e9d19053c0b57369167aa
kazi kuete charnel <chanelkuete2@gmail.com>
2023-09-21 12:19:27 +0100

    wip

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,30 @@
-
-function calculNombreJour(Nj,month,annee){
-    month = Number(document.getElementById("mois").value)
-    annee = Number(document.getElementById("annee").value)
-    switch(month){
-        case 1:
-            case 3:
-            case 5:
-            case 7:
-            case 8:
-                case 10:
-                    case 12:
-                        Nj= 31;
-                        break;
-                        case 4:
-                        case 6:
-                        case 9:
-                            case 11:
-                                Nj = 30;
-                                break;
-                                case 2:
-                                    if(annee%4==0){
-                                        Nj= 29 
-                                    }else{
-                                        Nj=28
-                                    }
-    }
-        document.querySelector("#valider").value = "le nombre de jour est " + Nj
-
+function calculNombreJour(Nj, month, annee) {
+  month = Number(document.getElementById("mois").value);
+  annee = Number(document.getElementById("annee").value);
+  switch (month) {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+      Nj = 31;
+      break;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      Nj = 30;
+      break;
+    case 2:
+      if (annee % 4 == 0) {
+        Nj = 29;
+      } else {
+        Nj = 28;
+      }
+  }
+  document.querySelector("#valider").value = "le nombre de jour est " + Nj;
 }
-let btn = document.querySelector("#submit")
-btn.addEventListener("click",calculNombreJour)
-
-
-
-
-
+let btn = document.querySelector("#submit");
+btn.addEventListener("click", calculNombreJour);
