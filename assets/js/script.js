e321525c4b8fdcf6c147c433d7ddd8b5bf670c49
kazi kuete charnel <chanelkuete2@gmail.com>
2023-09-21 08:32:01 +0100

    wip

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
 
 
-function calcul(DC1,DC2,Ds,result) {
-    DC1 = Number(document.getElementById("number1").value);
-    DC2 = Number(document.getElementById("number2").value);
-     Ds = Number(document.getElementById("number3").value);
- 
-    result = DC1 + DC2 + Ds;
-   document.getElementById("resultat").value= result;
- 
+function sum() {
+  
+  const numberOne = Number(document.getElementById("number1").value);
+  const numberTwo = Number(document.getElementById("number2").value);
+  const numberThree = Number(document.getElementById("number3").value);
+  const Result = numberOne + numberTwo + numberThree;
+  document.getElementById("result").value= Result;
  }
+ 
  let btn = document.querySelector("#send");
- btn.addEventListener("click", calcul);
+ btn.addEventListener("click", sum);
