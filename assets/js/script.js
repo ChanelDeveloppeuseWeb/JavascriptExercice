19ad07b949c5088ffdf9106bd3a796e2958697f2
kazi kuete charnel <chanelkuete2@gmail.com>
2023-09-21 05:38:03 +0100

    gg

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,20 @@
-function comparaisonNumber(DC1,DC2,Ds,result,comparaison) {
-   DC1 = Number(document.getElementById("number1").value);
-   DC2 = Number(document.getElementById("number2").value);
-    Ds = Number(document.getElementById("number3").value);
+function comparaisonNumber(DC1, DC2, Ds, result, comparaison) {
+  DC1 = Number(document.getElementById("number1").value);
+  DC2 = Number(document.getElementById("number2").value);
+  Ds = Number(document.getElementById("number3").value);
 
-    switch (comparaison) {
-      case (DC1===DC2 && DC2===Ds):
-        result="DC1,DC2,Ds sont egales"
-        
-        break;
-      case (DC1==DC2 || DC2==Ds);
+  switch (comparaison) {
+    case DC1 === DC2 && DC2 === Ds:
+      result = `les trois variables sont identiques`;
       break;
-      default:
-        break;
-    }
-   result = DC1 + DC2 + Ds;
-  document.getElementById("resultat").value= result;
-
+    case DC1 == DC2 || DC2 == Ds:
+      result = `deux des variables sont de valeurs egales`;
+      break;
+    default:
+      result = `les trois variable sont differentes`;
+      break;
+  }
+  document.getElementById("resultat").value = `${result}`;
 }
 let btn = document.querySelector("#send");
 btn.addEventListener("click", comparaisonNumber);
-
