c270c9868ae12048e0b15f910b9127cbad18dec6
kazi kuete charnel <chanelkuete2@gmail.com>
2023-09-21 12:24:24 +0100

    wip

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,8 @@ function sum() {
   const numberTwo = Number(document.getElementById("number2").value);
   const numberThree = Number(document.getElementById("number3").value);
   const Result = numberOne + numberTwo + numberThree;
-  document.getElementById("result").value= Result;
+  Somme = Result/3
+  document.getElementById("result").value= Somme;
  }
- 
  let btn = document.querySelector("#send");
  btn.addEventListener("click", sum);
