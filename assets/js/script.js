c16ff42c77ed110afd7c69b718fa45339125cd9d
kazi kuete charnel <chanelkuete2@gmail.com>
2023-09-22 14:11:58 +0100

    wip

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,28 @@
-function valeurNonAlbrege() {
-   quantites = Number(document.getElementById("number1").value);
-   unit = Number(document.getElementById("number2").value);
-   result = DC1;
-  document.getElementById("resultat").value= result;
-
+function convertAlgebraValue() {
+const  quantity =document.getElementById("number1").value;
+let unit = quantity * 1024;
+   switch (unit) {
+    case ("K"):
+      result = quantity * 1024;
+      break;
+    case "M":
+      result = quantity * 1024;
+      break;
+    case "G":
+      result = quantity * 1024;
+      break;
+    case "T":
+      result = quantity * 1024;
+      break;
+    default:
+      result = "Unité invalide";
+      break;
+  }
+console.log(unit);
+  document.getElementById("resultat").value = unit;
 }
 let btn = document.querySelector("#send");
-btn.addEventListener("click",valeurNonAlbrege );
+btn.addEventListener("click", convertAlgebraValue);
 
+// la methode parseInt permet de convertir la valeur en un type de donnee numerique (entier)
+//toString convertir un entier en une chaine de caractere
