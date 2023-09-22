075e034d81d0665e4021fd10ff97debb45c6b25c
kazi kuete charnel <chanelkuete2@gmail.com>
2023-09-22 18:48:23 +0100

    wip

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,27 @@
 function nombreAbregee() {
-  let quantity = document.getElementById("number1").value;
-  let unit;
-  switch (unit) {
-    case "K":
+  let quantity = Number(document.getElementById("number1").value);
+  let unit = "k";"M";"G";"T";
+  //  let  result = 0;
+  switch (unit.toUpperCase()) {
+    case ("K"):
       result = quantity * 1024;
       break;
-    case "M":
+    case ("M"):
       result = quantity * 1024 * 1024;
       break;
-    case "G":
+    case ("G"):
       result = quantity * 1024 * 1024 * 1024;
       break;
-    case "T":
+    case ("T"):
       result = quantity * 1024 * 1024 * 1024 * 1024;
       break;
 
     default:
-      result = `${quantity}K`.trimEnd();
+      result = `${quantity}k`.trimEnd();
       break;
+      
   }
   document.querySelector("#resultat").value = result;
 }
-let btn = document.getElementById("send");
-btn.addEventListener("click", nombreAbregee);
+// let btn = document.getElementById("send");
+// btn.addEventListener("click", nombreAbregee());
